260235cbb40da70546ed9beb7e4cc2aadcb4c883
FukeKazki <kazkichi0906@gmail.com>
2019-05-21 21:09:35 +0900

    Add: HTMLファイルの追加

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>福井県イベント情報サイト</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="Description" content="福井県イベント情報サイトです。">
+    <meta name="Keywords" content="福井, イベント">
+    <link rel="stylesheet" href="./com/css/import.css">
+    <link rel="stylesheet" href="./com/js/vegas/vegas.min.css">
+</head>
+<body>
+<header>
+    <div class="hc">
+        <h1>福井県イベント情報サイト</h1>
+    </div>
+</header>
+<!--<div class="hero-img">-->
+<!--    <div class="hero-mask">-->
+<!--        <div class="hero-title"><h1>Welcome to Fukui</h1></div>-->
+<!--    </div>-->
+<!--</div>-->
+<main>
+    <div class="mc">
+        <h2>イベント情報</h2>
+        <div class="calendar">
+            calendar
+        </div>
+        <div class="forms">
+            <h3>キーワード検索</h3>
+            <input type="text" value="キーワード">
+            <button>検索</button>
+            <h3>カテゴリ検索</h3>
+            <select name="" id="">
+                <option value="cal">カテゴリ1</option>
+                <option value="cal">カテゴリ2</option>
+                <option value="cal">カテゴリ3</option>
+            </select>
+            <h3>表示検索</h3>
+            <select name="showItems" id="showItems">
+                <option value="10">10件表示</option>
+                <option value="20">20件表示</option>
+                <option value="30">30件表示</option>
+                <option value="99">全件表示</option>
+            </select>
+        </div>
+        <div class="events">
+        </div>
+    </div>
+</main>
+<footer>
+    <div class="fc">
+        <h3>フッター</h3>
+    </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="./com/js/vegas/vegas.min.js"></script>
+<script src="./com/js/index.js"></script>
+</body>
+</html>
